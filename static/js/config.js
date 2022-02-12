5bbdfe084a3418f24db80e31ef8685522dd8ab5b
ARM <armck.phantom@gmail.com>
2022-02-12 04:11:33 -0600

    fix mapbox api

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYXJtY2siLCJhIjoiY2s3cnA4emV1MDV0bjNlbzV5MHV2ZzQzNyJ9.kMQ-hRjKZQof9z4eniqWwA";
+const API_KEY = "pk.eyJ1IjoiYXJtY2siLCJhIjoiY2t6am55cGxtMGJxczJ1bXFqZHV1YWVhcCJ9.4tQy-1PXro6rmfV8L-Q2XA";
